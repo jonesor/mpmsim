baaea09db1a799863e60088370def2cea0ee0fea
Owen Jones <jonesor@gmail.com>
2023-01-18 21:55:19 +0100

    Create test-simulate_mpm.R

---
+++ tests/testthat/test-simulate_mpm.R
@@ -0,0 +1,57 @@
+# expect matU to be a matrix
+testthat::expect_error(
+  simulate_mpm(
+    matU = rep(0.1, 4), matF = matrix(rep(0.1, 4), ncol = 2),
+    sample_size = matrix(rep(10, 4), ncol = 2), split = TRUE
+  )
+)
+
+# expect matF to be a matrix
+testthat::expect_error(
+  simulate_mpm(
+    matU = matrix(rep(0.1, 4), ncol = 2), matF = rep(0.1, 4),
+    sample_size = matrix(rep(10, 4), ncol = 2), split = TRUE
+  )
+)
+
+# expect sample_size to be a matrix, or a single integer > 0
+testthat::expect_error(
+  simulate_mpm(
+    matU = matrix(rep(0.1, 4), ncol = 2), matF = matrix(rep(0.1, 4), ncol = 2),
+    sample_size = c(1, 2), split = TRUE
+  )
+)
+
+
+# expect matU to be >= 0
+testthat::expect_error(
+  simulate_mpm(
+    matU = matrix(rep(-0.1, 4), ncol = 2), matF = matrix(rep(0.1, 4), ncol = 2),
+    sample_size = matrix(rep(10, 4), ncol = 2), split = TRUE
+  )
+)
+
+
+# expect matF to be >= 0
+testthat::expect_error(
+  simulate_mpm(
+    matU = matrix(rep(0.1, 4), ncol = 2), matF = matrix(rep(-0.1, 4), ncol = 2),
+    sample_size = matrix(rep(10, 4), ncol = 2), split = TRUE
+  )
+)
+
+# expect sample_size to be > 0
+testthat::expect_error(
+  simulate_mpm(
+    matU = matrix(rep(0.1, 4), ncol = 2), matF = matrix(rep(0.1, 4), ncol = 2),
+    sample_size = matrix(rep(-10, 4), ncol = 2), split = TRUE
+  )
+)
+
+# expect sample_size to be integers
+testthat::expect_error(
+  simulate_mpm(
+    matU = matrix(rep(0.1, 4), ncol = 2), matF = matrix(rep(0.1, 4), ncol = 2),
+    sample_size = matrix(rep(10.5, 4), ncol = 2), split = TRUE
+  )
+)
