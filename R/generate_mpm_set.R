b3dcb4c3865c81fd7d05674d4d3f933781c6a217
Owen Jones <jonesor@gmail.com>
2023-04-08 20:24:03 +0200

    Update generate_mpm_set.R
    Function can now constrain outputs to any criteria, based on outputs from an A matrix.

--- R/generate_mpm_set.R
+++ R/generate_mpm_set.R
@@ -2,16 +2,15 @@
 #' history archetypes
 #'
 #' This function generates a list of `n` MPMs according to the specified
-#' criteria. Criteria include the `archetype`, and the acceptable lambda values.
-#' The function attempts to find matrices that fulfill the lambda criteria,
-#' discarding unacceptable matrices. If it takes more than 1000 attempts to find
-#' a suitable matrix model, then an error is produced.
+#' criteria. Criteria include the `archetype`, and the acceptable constraining
+#' criteria, which could include lambda, generation time or any other metric
+#' derived from an A matrix.
+#' The function attempts to find matrices that fulfill the criteria, discarding
+#' unacceptable matrices. By default, if it takes more than 1000 attempts to
+#' find a suitable matrix model, then an error is produced. However, the number
+#' of attempts can be altered with the `attempts` parameter.
 #'
 #' @param n The number of MPMs to generate. Default is 10.
-#' @param lower_lambda The lower bound for acceptable lambda values. Default is
-#'   0.9.
-#' @param upper_lambda The upper bound for acceptable lambda values. Default is
-#'   1.1.
 #' @param n_stages The number of stages for the MPMs. Default is 3.
 #' @param archetype The archetype of the MPMs. Default is 1.
 #' @param fecundity A vector of fecundities for the MPMs. Default is 1.5.
@@ -22,58 +21,69 @@
 #'   also be `FALSE`.
 #' @param max_surv The maximum acceptable survival value. Defaults to 0.99. This
 #'   is only used if `split = TRUE`.
-#' @param constraint An optional list specifying (1) a function that outputs a metric
-#'   derived from an A matrix and (2) acceptable bounds that are used to
-#'   constrain matrix outputs. E.g. `list("generation.time", c(5,10))` to
-#'   specify that `generate_mpm_set` should only output MPMs with a generation
-#'   time between 5 and 10. Default is `NULL`.
+#' @param constraint An optional data frame with 4 columns named `fun`, `arg`,
+#'   `lower` and `upper`. These columns specify (1) a function that outputs a
+#'   metric derived from an A matrix and (2) an argument for the function (`NA`,
+#'   if no argument supplied) (3) the lower acceptable bound for the metric and
+#'   (4) upper acceptable bound for the metric. This could be used to specify
 #' @param attempts an integer indicating the number of attempts To be made when
 #'   simulating matrix model. The default is 1000. If it takes more than 1000
 #'   attempts to make a matrix that satisfies the conditions set by the other
 #'   arguments, then a warning is produced.
 #' @return A list of MPMs that meet the specified criteria.
 #'
-#' @importFrom popdemo eigs
+#' @importFrom popdemo isErgodic
 #' @author Owen Jones <jones@biology.sdu.dk>
 #' @examples
+#'
+#' # Basic operation, without splitting matrices and with no constraints
 #' generate_mpm_set(
-#'   n = 10, lower_lambda = 0.9, upper_lambda = 1.1,
-#'   n_stages = 5, fecundity = c(0, 0, 4, 8, 10), archetype = 4, split = TRUE
+#'   n = 10, n_stages = 5, fecundity = c(0, 0, 4, 8, 10),
+#'   archetype = 4, split = FALSE
 #' )
 #'
+#' # Constrain outputs to A matrices with lambda between 0.9 and 1.1
 #' library(popbio)
+#' constrain_df <- data.frame(fun = "lambda", arg = NA, lower = 0.9, upper = 1.1)
 #' generate_mpm_set(
-#'   n = 1, lower_lambda = 0.9, upper_lambda = 1.1,
-#'   n_stages = 5, fecundity = c(0, 0, 4, 8, 10), archetype = 4, split = TRUE,
-#'   constraint = list("generation.time", c(40, 50))
+#'   n = 10, n_stages = 5, fecundity = c(0, 0, 4, 8, 10),
+#'   archetype = 4, constraint = constrain_df
+#' )
+#'
+#' # As above, but using popdemo::eigs function instead of popbio::lambda
+#' # to illustrate use of argument
+#' library(popdemo)
+#' constrain_df <- data.frame(fun = "eigs", arg = "lambda", lower = 0.9, upper = 1.1)
+#' generate_mpm_set(
+#'   n = 10, n_stages = 5, fecundity = c(0, 0, 4, 8, 10),
+#'   archetype = 4, constraint = constrain_df
+#' )
+#'
+#' # Multiple constraints
+#' # Constrain outputs to A matrices with lambda between 0.9 and 1.1, generation
+#' # time between 3 and 5 and damping ratio between 1 and 7.
+#' library(popbio)
+#' constrain_df <- data.frame(
+#'   fun = c("lambda", "generation.time", "damping.ratio"),
+#'   arg = c(NA, NA, NA),
+#'   lower = c(0.9, 3.0, 1.0),
+#'   upper = c(1.1, 5.0, 7.0)
+#' )
+#' generate_mpm_set(
+#'   n = 10, n_stages = 5, fecundity = c(0, 0, 4, 8, 10),
+#'   archetype = 4, constraint = constrain_df
 #' )
 #'
 #' @seealso [random_mpm()] which this function is essentially a wrapper for.
 #' @family simulation
 #' @export generate_mpm_set
 
-generate_mpm_set <- function(n = 10, lower_lambda = 0.9, upper_lambda = 1.1,
-                             n_stages = 3, archetype = 1, fecundity = 1.5,
+generate_mpm_set <- function(n = 10, n_stages = 3, archetype = 1, fecundity = 1.5,
                              split = TRUE, by_type = TRUE, max_surv = 0.99,
                              constraint = NULL, attempts = 1000) {
   # Check if n is a positive integer
   if (!min(abs(c(n %% 1, n %% 1 - 1))) < .Machine$double.eps^0.5 || n <= 0) {
     stop("n must be a positive integer")
-  }
-
-  # Check if lower_lambda is positive and less than upper_lambda
-  if (lower_lambda <= 0 || lower_lambda >= upper_lambda) {
-    stop("lower_lambda must be positive and less than upper_lambda")
-  }
-
-  # Check if upper_lambda is positive and greater than lower_lambda
-  if (upper_lambda <= 0) {
-    stop("upper_lambda must be positive")
-  }
-
-  # Check if upper_lambda is positive and greater than lower_lambda
-  if (upper_lambda <= lower_lambda) {
-    stop("upper_lambda must greater than lower_lambda")
   }
 
   if (split == FALSE && by_type == TRUE) {
@@ -90,17 +100,6 @@ generate_mpm_set <- function(n = 10, lower_lambda = 0.9, upper_lambda = 1.1,
       fecundity = fecundity, split = split
     )
 
-    # Get lambda value
-    if (split == TRUE) {
-      lambda_value <- eigs(mpm_out$mat_U + mpm_out$mat_F, what = "lambda")
-    } else {
-      lambda_value <- eigs(mpm_out, what = "lambda")
-    }
-
-    # Check whether lambda value is acceptable
-    lambda_value_accepted <- lambda_value < upper_lambda &
-      lambda_value > lower_lambda
-
     # Check whether survival values are acceptable
     if (split == TRUE) {
       survival_value_accepted <- max(colSums(mpm_out$mat_U)) < max_surv
@@ -108,31 +107,46 @@ generate_mpm_set <- function(n = 10, lower_lambda = 0.9, upper_lambda = 1.1,
       survival_value_accepted <- TRUE
     }
 
-    # CHeck whehter optional function output is acceptavle
+    # Check whether optional function output is acceptable
 
     if (is.null(constraint)) {
       fun_output_accepted <- TRUE
     }
 
     if (!is.null(constraint)) {
-      # Apply function to the A matrix
-      FUN <- match.fun(constraint[[1]])
+      nConstraints <- nrow(constraint)
+      constraint_OK <- rep(NA, nConstraints)
 
       if (split == TRUE) {
-        fun_output <- FUN(mpm_out$mat_U + mpm_out$mat_F)
+        mat_A_temp <- mpm_out$mat_U + mpm_out$mat_F
       } else {
-        fun_output <- FUN(mpm_out)
+        mat_A_temp <- mpm_out
       }
-      # Check the output is within the given bounds
-      lower_bound <- constraint[[2]][1]
-      upper_bound <- constraint[[2]][2]
 
-      fun_output_accepted <- fun_output < upper_bound &
-        fun_output > lower_bound
+      for (k in 1:nConstraints) {
+        constraint_focal <- constraint[k, ]
+        constraint_function <- match.fun(constraint_focal[, 1])
+
+        if (is.na(constraint_focal[, 2])) {
+          args_list <- list(mat_A_temp)
+        } else {
+          args_list <- list(A = mat_A_temp, constraint_focal[, 2])
+        }
+        fun_output <- do.call(constraint_function, args_list)
+
+        # Check that the fun_output is acceptable.
+        lower_bound <- constraint_focal[3]
+        upper_bound <- constraint_focal[4]
+
+        fun_output_accepted <- fun_output < upper_bound &
+          fun_output > lower_bound
+
+        constraint_OK[k] <- fun_output_accepted
+      }
     }
 
-    if (lambda_value_accepted && survival_value_accepted && fun_output_accepted) {
-      # if the lambda is acceptable, add the matrix to the output_list
+    if (survival_value_accepted && fun_output_accepted) {
+      # if values are acceptable, add the matrix to the output_list
       # (otherwise do nothing)
 
       # If this is the first attempt, and the list is all NULL, then set i = 1
