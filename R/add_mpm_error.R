5fd27c3b574053e9da6cc60085c004efd6470e6c
Owen Jones <jonesor@gmail.com>
2023-04-09 20:26:12 +0200

    add_mpm_error
    add wrapper function for add_mpm_error to apply error to lists  of matrices

--- R/add_mpm_error.R
+++ R/add_mpm_error.R
@@ -40,19 +40,19 @@ simulate_fecundity <- function(mean_fecundity, sample_size) {
   }
 }
 
-#' Simulate a matrix population model (MPM) with sampling error based on
-#' expected values of transition rates and sample sizes
+#' Add sampling error to a matrix population model (MPM) based on expected
+#' values of transition rates and sample sizes
 #'
-#' Simulates a matrix population model based on expected values in the
-#' transition matrix. The expected values are provided in two matrices `mat_U`
-#' for the growth/development and survival transitions and `mat_F` for the
-#' fecundity transitions. The `mat_U` values are simulated based on expected
-#' probabilities, assuming a binomial process with a sample size defined by
-#' `sample_size`. The `mat_F` values are simulated using a Poisson process with
-#' a sample size defined by `sample_size`.Thus users can expect that large
-#' sample sizes will result in simulated matrices that match closely with the
-#' expectations, while simulated matrices with small sample sizes will be more
-#' variable.
+#' Produces a matrix population model (MPM) based on expected values in the
+#' transition matrix and sample size. The expected values are provided in two
+#' submatrices `mat_U` for the growth/development and survival transitions and
+#' `mat_F` for the fecundity transitions. The output `mat_U` values are
+#' simulated based on expected probabilities, assuming a binomial process with a
+#' sample size defined by `sample_size`. The output `mat_F` values are simulated
+#' using a Poisson process with a sample size defined by `sample_size`.Thus
+#' users can expect that large sample sizes will result in simulated matrices
+#' that match closely with the expectations, while simulated matrices with small
+#' sample sizes will be more variable.
 #'
 #' @param mat_U matrix of mean survival/growth probabilities
 #' @param mat_F matrix of mean fecundity values
@@ -77,13 +77,13 @@ simulate_fecundity <- function(mean_fecundity, sample_size) {
 #' ssMat <- matrix(10, nrow = 3, ncol = 3)
 #'
 #' # Sample size is a single matrix
-#' add_mpm_error(
+#' add_mpm_error_indiv(
 #'   mat_U = mats$mat_U, mat_F = mats$mat_F,
 #'   sample_size = ssMat, split = TRUE
 #' )
 #'
 #' # Sample size is a single value
-#' add_mpm_error(
+#' add_mpm_error_indiv(
 #'   mat_U = mats$mat_U, mat_F = mats$mat_F,
 #'   sample_size = 50, split = TRUE
 #' )
@@ -94,14 +94,15 @@ simulate_fecundity <- function(mean_fecundity, sample_size) {
 #'   "mat_U_ss" = matrix(10, nrow = 3, ncol = 3)
 #' )
 #'
-#' add_mpm_error(
+#' add_mpm_error_indiv(
 #'   mat_U = mats$mat_U, mat_F = mats$mat_F,
 #'   sample_size = ssMats, split = TRUE
 #' )
 #'
-#' @export add_mpm_error
+#' @export add_mpm_error_indiv
+#' @noRd
 #'
-add_mpm_error <- function(mat_U, mat_F, sample_size, split = TRUE) {
+add_mpm_error_indiv <- function(mat_U, mat_F, sample_size, split = TRUE) {
   # Validation
   if (!inherits(mat_U, "matrix")) {
     stop("mat_U needs to be a matrix")
@@ -228,8 +229,162 @@ add_mpm_error <- function(mat_U, mat_F, sample_size, split = TRUE) {
   )
 
   if (split) {
-    return(list(mat_U = mat_U_out, mat_F = mat_F_out))
+    return(list(mat_A = mat_U_out + mat_F_out,
+                mat_U = mat_U_out,
+                mat_F = mat_F_out))
   } else {
     return(mat_U_out + mat_F_out)
+  }
+}
+
+
+
+#' Add sampling error to matrix population models (MPMs) based on expected
+#' values of transition rates and sample sizes
+#'
+#' Produces a list of matrix population models based on expected values in the
+#' transition matrix and sample size. The expected values are provided in lists
+#' of two submatrices: `mat_U` for the growth/development and survival
+#' transitions and `mat_F` for the fecundity transitions. The output `mat_U`
+#' values are simulated based on expected probabilities, assuming a binomial
+#' process with a sample size defined by `sample_size`. The output `mat_F`
+#' values are simulated using a Poisson process with a sample size defined by
+#' `sample_size`.Thus users can expect that large sample sizes will result in
+#' simulated matrices that match closely with the expectations, while simulated
+#' matrices with small sample sizes will be more variable.
+#'
+#' @param mat_U A list of U submatrices, or a single U submatrix.
+#' @param mat_F A list of F submatrices, or a single F submatrix.
+#' @param sample_size either (1) a single matrix of sample sizes for each
+#'   element of every MPM, (2) a list of two named matrices ("`mat_F_ss`",
+#'   "`mat_U_ss`") containing sample sizes for the survival and fertility
+#'   submatrices of every MPM or (3) a single value applied to the every element
+#'   of every matrix.
+#' @param split logical, whether to split the output into survival and fecundity
+#'   matrices or not. Defaults to `TRUE`.
+#' @param by_type A logical indicating whether the matrices should be returned
+#'   in a list by type (A, U, F, C). If split is `FALSE`, then `by_type` must
+#'   also be `FALSE`. Defaults to `TRUE`.
+#' @return list of matrices of survival and fecundity if `split = TRUE`,
+#'   otherwise a single matrix of the sum of survival and fecundity.
+#' @details if any `sample_size` input is 0, it is assumed that the estimate for
+#'   the element(s) concerned is known without error.
+#' @family errors
+#'
+#' @examples
+#' # First generate a set of MPMs
+#' mpm_set <- generate_mpm_set(n = 5, n_stages = 5, fecundity = c(
+#'   0, 0, 4, 8,
+#'   10
+#' ), archetype = 4, split = TRUE, by_type = TRUE)
+#'
+#' # Now apply sampling error to this set
+#' add_mpm_error(mat_U = mpm_set$U_list, mat_F = mpm_set$F_list,sample_size =
+#' 50)
+#'
+#' # Also works with a single matrix.
+#' mats <- make_leslie_mpm(
+#'   survival = c(0.1, 0.2, 0.5),
+#'   fertility = c(0, 1.2, 2.4),
+#'   n_stages = 3, split = TRUE
+#' )
+#'
+#' # Sample size is a single value
+#' add_mpm_error(mat_U = mats$mat_U, mat_F = mats$mat_F, sample_size = 20)
+#'
+#' # Sample size is a list of two matrices
+#' # here with a sample size of 20 for reproduction and 10 for growth/survival.
+#' mpm_set <- generate_mpm_set(
+#'   n = 5, n_stages = 3, fecundity = c(0, 2, 4),
+#'   archetype = 4, split = TRUE, by_type = TRUE
+#' )
+#'
+#' ssMats <- list(
+#'   "mat_F_ss" = matrix(20, nrow = 3, ncol = 3),
+#'   "mat_U_ss" = matrix(10, nrow = 3, ncol = 3)
+#' )
+#'
+#' # Add sampling error to the matrix models
+#' output <- add_mpm_error(mat_U = mpm_set$U_list, mat_F = mpm_set$F_list,
+#' sample_size = ssMats)
+#'
+#' #Examine the outputs
+#' names(output)
+#' output
+#'
+#' @author Owen Jones <jones@biology.sdu.dk>
+#' @family errors
+#'
+#' @export add_mpm_error
+#'
+
+add_mpm_error <- function(mat_U, mat_F, sample_size, split = TRUE,
+                          by_type = TRUE) {
+  # Validate mat_U
+  if (!inherits(mat_U, "list") && !inherits(mat_U, "matrix")) {
+    stop("mat_U must be either a matrix or a list of matrices.")
+  }
+
+  if (inherits(mat_U, "list")) {
+    for (i in 1:length(mat_U)) {
+      if (!inherits(mat_U[[i]], "matrix")) {
+        stop("Each element of mat_U must be a matrix.")
+      }
+    }
+  }
+  # Validate mat_F
+  if (!inherits(mat_F, "list") && !inherits(mat_F, "matrix")) {
+    stop("mat_F must be either a matrix or a list of matrices.")
+  }
+
+  if (inherits(mat_F, "list")) {
+    for (i in 1:length(mat_F)) {
+      if (!inherits(mat_F[[i]], "matrix")) {
+        stop("Each element of mat_F must be a matrix.")
+      }
+    }
+  }
+
+  if (split == FALSE && by_type == TRUE) {
+    stop("If split is FALSE, then by_type must also be FALSE")
+  }
+
+  # If mat_U is a single matrix, add it to a list with one element
+  if (inherits(mat_U, "matrix")) {
+    mat_U <- list(mat_U)
+  }
+  # If mat_F is a single matrix, add it to a list with one element
+  if (inherits(mat_F, "matrix")) {
+    mat_F <- list(mat_F)
+  }
+
+  # Make a blank list to hold the output
+  output_list <- list()
+  for (i in 1:length(mat_U)) {
+    output_list[[i]] <- add_mpm_error_indiv(
+      mat_U = mat_U[[i]],
+      mat_F = mat_F[[i]],
+      sample_size = sample_size,
+      split = split
+    )
+  }
+
+  if (length(output_list) == 1) {
+    return(output_list[[1]])
+  } else {
+    if (by_type == TRUE) {
+      A_list <- lapply(output_list, function(x) x$mat_A)
+      U_list <- lapply(output_list, function(x) x$mat_U)
+      F_list <- lapply(output_list, function(x) x$mat_F)
+      mpm_output_by_type <- list(
+        "A_list" = A_list,
+        "U_list" = U_list,
+        "F_list" = F_list
+      )
+      return(mpm_output_by_type)
+    }
+    if (by_type == FALSE) {
+      return(output_list)
+    }
   }
 }
