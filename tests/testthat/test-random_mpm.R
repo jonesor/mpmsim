8c50b71a764692db5b67e6bae8eb75a071edf517
Owen Jones <jonesor@gmail.com>
2023-06-14 15:48:53 +0200

    Update test-random_mpm.R

--- tests/testthat/test-random_mpm.R
+++ tests/testthat/test-random_mpm.R
@@ -96,3 +96,126 @@ x <- random_mpm(
 testthat::expect_true(
   inherits(x, "matrix")
 )
+
+fec_mat <- matrix(c(
+  0, 1, 70,
+  0, 0, 0,
+  0, 0, 0
+), ncol = 3, byrow = TRUE)
+testthat::expect_error(
+  random_mpm(n_stages = 2, fecundity = fec_mat, archetype = 1, split = FALSE)
+)
+
+
+#Lower limit to fecundity
+fec_mat_lower <- matrix(c(0,1,70,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Upper limit to fecundity
+fec_mat_upper <- matrix(c(0,5,100,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Place the two matrices in a list
+fec_mats <- list(fec_mat_lower,fec_mat_upper)
+
+testthat::expect_error(
+  random_mpm(n_stages = 2, fecundity = fec_mats, archetype = 1, split = FALSE)
+)
+
+
+fec_mat <- matrix(c(
+  0, -1, 70,
+  0, 0, 0,
+  0, 0, 0
+), ncol = 3, byrow = TRUE)
+testthat::expect_error(
+  random_mpm(n_stages = 3, fecundity = fec_mat, archetype = 1, split = FALSE)
+)
+
+#Lower limit to fecundity
+fec_mat_lower <- matrix(c(0,5,70,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Upper limit to fecundity
+fec_mat_upper <- matrix(c(0,1,100,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Place the two matrices in a list
+fec_mats <- list(fec_mat_lower,fec_mat_upper)
+
+testthat::expect_error(
+  random_mpm(n_stages = 3, fecundity = fec_mats, archetype = 1, split = FALSE)
+)
+
+# Check bounds of fecundity matrices
+
+#Lower limit to fecundity
+fec_mat_lower <- matrix(c(0,5,70,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Upper limit to fecundity
+fec_mat_upper <- matrix(c(0,1,100,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Place the two matrices in a list
+fec_mats <- list(fec_mat_lower,fec_mat_upper)
+
+testthat::expect_error(
+  random_mpm(n_stages = 3, fecundity = fec_mats, archetype = 1, split = FALSE)
+)
+
+
+#Lower limit to fecundity
+fec_mat_lower <- matrix(c(0,5,70,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Upper limit to fecundity
+fec_mat_upper <- matrix(c(0,1,100,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Place the two matrices in a list
+fec_mats <- list(fec_mat_lower,fec_mat_upper, fec_mat_upper)
+
+testthat::expect_error(
+  random_mpm(n_stages = 3, fecundity = fec_mats, archetype = 1, split = FALSE)
+)
+
+#Lower limit to fecundity
+fec_mat_lower <- matrix(c(0,5,70,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Upper limit to fecundity
+fec_mat_upper <- matrix(c(0,10,100,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+#Place the two matrices in a list
+fec_mats <- list(fec_mat_lower,fec_mat_upper)
+
+
+temp <- random_mpm(n_stages = 3, fecundity = fec_mats, archetype = 1, split = FALSE)
+
+testthat::expect_true(inherits(temp, "matrix"))
+
+
+
+#Lower limit to fecundity
+fec_mat <- matrix(c(0,5,70,
+                          0,0,0,
+                          0,0,0), ncol = 3, byrow = TRUE)
+
+
+temp <- random_mpm(n_stages = 3, fecundity = fec_mat, archetype = 1, split = FALSE)
+
+testthat::expect_true(inherits(temp, "matrix"))
+
+
